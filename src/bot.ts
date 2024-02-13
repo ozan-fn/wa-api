eb3808b1fef11410f7868adcba25f03254e5c18c
23SA11A032 <23SA11A032@students.amikompurwokerto.ac.id>
2024-02-13 12:33:37 +0000

    _

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,23 @@
-import makeWASocket, { Browsers, UserFacingSocketConfig, WASocket, useMultiFileAuthState } from "@whiskeysockets/baileys";
+import makeWASocket, {
+    Browsers,
+    UserFacingSocketConfig,
+    WASocket,
+    makeCacheableSignalKeyStore,
+    useMultiFileAuthState,
+} from "@whiskeysockets/baileys";
 import pino, { Logger } from "pino";
 
 var conn: WASocket;
+const logger = pino({ level: "silent" });
 
 async function connect() {
     if (conn) return conn;
     const { state, saveCreds } = await useMultiFileAuthState(__dirname + "/session");
-    conn = makeWASocket({ printQRInTerminal: true, auth: state, browser: Browsers.macOS("Desktop"), syncFullHistory: true });
+    conn = makeWASocket({
+        printQRInTerminal: true,
+        auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, logger) },
+        logger: logger,
+    });
     conn.ev.on("creds.update", saveCreds);
     return conn;
 }
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Router from "@koa/router";
 import { bodyParser } from "@koa/bodyparser";
 import { connect } from "./bot";
 import * as emoji from "node-emoji";
+import { WASocket } from "@whiskeysockets/baileys";
 
 const app = new Koa();
 const router = new Router();
@@ -12,13 +13,29 @@ app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+let bot: WASocket;
+
+async function connectBot() {
+    let connected = false;
+    while (!connected) {
+        try {
+            bot = await connect();
+            connected = true;
+            console.log("Bot connected successfully");
+        } catch (error) {
+            console.error("Failed to connect bot, retrying...", error);
+            await new Promise((resolve) => setTimeout(resolve, 5000)); // Tunda  5 detik sebelum mencoba lagi
+        }
+    }
+}
+
 async function main() {
-    var bot = await connect();
+    await connectBot(); // Memastikan bot terhubung sebelum melanjutkan
 
     bot.ev.on("connection.update", async (conn) => {
-        if (conn.connection == "close") {
-            bot.end(undefined);
-            bot = await connect();
+        if (conn.connection === "close") {
+            console.log("Connection closed, attempting to reconnect...");
+            await connectBot(); // Coba terhubung ulang saat koneksi ditutup
         }
     });
 
@@ -26,6 +43,7 @@ async function main() {
         const { phoneNumber, text } = ctx.request.body;
         try {
             const response = await bot.sendMessage(`${phoneNumber}@s.whatsapp.net`, { text: emoji.emojify(text) });
+            console.log(phoneNumber, response?.status);
             ctx.body = { success: true, response };
         } catch (error) {
             if (error instanceof Error) {
